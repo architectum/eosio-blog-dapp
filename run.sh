f4ca1fa480ccf0eeea757af49b04ba02cc72097a
onegin 🚀 <evgeniyivanets@gmail.com>
2018-10-26 07:13:47 +0300

    docker env

--- run.sh
+++ run.sh
@@ -1,40 +1,46 @@
 #!/usr/bin/env bash
-set -o errexit
-echo "=== start ==="
-cd "$(dirname "$0")"
-if [ ! -x "$(command -v docker)" ] ||
-   [ ! -x "$(command -v node)" ]; then
-    echo ""
-    echo -e "\033[0;31m[Error with Exception]\033[0m"
-    echo "Please make sure Docker and Node.js are installed"
-    echo ""
-    echo "Install Docker: https://docs.docker.com/install/"
-    echo "Install Node.js: https://nodejs.org/en/"
-    echo ""
-    exit
-fi
-echo "=== pull eosio/eos-dev image v1.3.2 from docker hub ==="
-echo "=== pull mongo image 4.0 from docker hub ==="
-docker-compose pull
-#
-echo "=== setup/reset data for 'blog' ==="
-rm -rf "./eosio_docker/data"
-mkdir -p "./eosio_docker/data"
-#
-echo "=== setup/reset data for 'mongo' ==="
-rm -rf "./storage_docker/mongo"
-mkdir -p "./storage_docker/mongo"
-#
-echo "=== setup/reset data for 'redis' ==="
-rm -rf "./storage_docker/redis"
-mkdir -p "./storage_docker/redis"
-#
-echo "=== npm install packpage for frontend react app ==="
-cd "./frontend"
-npm install
-cd "../"
-#
-echo "=== npm install packpage for frontend react app ==="
-cd "./backend"
-npm install
-cd "../"
+# set -e
+# echo "=== start ==="
+# cd "$(dirname "$0")"
+# if [ ! -x "$(command -v docker)" ] ||
+#    [ ! -x "$(command -v node)" ]; then
+#     echo ""
+#     echo -e "\033[0;31m[Error with Exception]\033[0m"
+#     echo "Please make sure Docker and Node.js are installed"
+#     echo ""
+#     echo "Install Docker: https://docs.docker.com/install/"
+#     echo "Install Node.js: https://nodejs.org/en/"
+#     echo ""
+#     exit
+# fi
+confirm_recreate() {
+    read -p "Are you sure? (y/n): " choice
+    case "$choice" in
+        y|Y ) rm -rf "$1" && mkdir -p "$1";;
+        n|N ) return 0;;
+        * ) exit 1;;
+    esac
+}
+
+recreate_storages() {
+    echo "=== Delete blockchain data for 'blog' [eosio/eos-dev] ? ==="
+    confirm_recreate "./eosio_docker/data"
+    echo "=== Delete 'mongodb' data [mongo] ? ==="
+    confirm_recreate "./storage_docker/mongo"
+    echo "=== Delete 'postgresql' data [postgres] ? ==="
+    confirm_recreate "./storage_docker/postgres";
+}
+
+read -p "Clear storage and blockchain data? (y/n): " choice
+case "$choice" in
+    y|Y ) recreate_storages;;
+    n|N ) return 0;;
+    * ) exit 1;;
+esac
+
+echo "=== pull images (eosio/eos-dev, mongo, postgres, adminer) from https://hub.docker.com ==="
+docker-compose up -d --remove-orphans --no-build
+
+# cd "./frontend" && npm install
+# cd "../backend" && npm install
+# cd "../"
