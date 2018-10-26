e265343d9eb9c588639546976fe7a5ba2c904fee
SophieKolesnyk <sophiekolesnik@gmail.com>
2018-10-26 08:46:07 +0300

    docker windows endings?

--- eosio_docker/docker/run.sh
+++ eosio_docker/docker/run.sh
@@ -1,6 +1,2 @@
-#!/usr/bin/env bash
-if [ -e "/mnt/dev/data/initialized" ]; then
-    source "/opt/eosio/bin/scripts/continue_blockchain.sh"
-else
-    source "/opt/eosio/bin/scripts/init_blockchain.sh"
-fi
+#!/usr/bin/env bash 
+if [ -e "/mnt/dev/data/initialized" ]; then source "/opt/eosio/bin/scripts/continue_blockchain.sh"; else source "/opt/eosio/bin/scripts/init_blockchain.sh"; fi
